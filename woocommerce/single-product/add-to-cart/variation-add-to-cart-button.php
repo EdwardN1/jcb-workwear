aabb894ff624dff9cb6c100d0c347dd1d3de1c59
EdwardN1 <GH?841241ed>
2019-04-26 16:29:28 +0100

    26-04-2019-10:42

--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -7,111 +7,121 @@
  * @version 3.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
-$sizeguide = get_field( 'a_sizing_guide', $product->get_id() );
-if ( $sizeguide ) {
-	?>
+$sizeguide = get_field('a_sizing_guide', $product->get_id());
+if ($sizeguide) {
+    ?>
     <div class="toprempad-1">
         <ul class="accordion size-guide" data-accordion data-allow-all-closed="true">
             <li class="accordion-item" data-accordion-item>
                 <!-- Accordion tab title -->
                 <a href="#" class="accordion-title"
                    style="border: none; background-color: #f2f2f2; font-size: 15px; color: #666666;">
-					<?php the_field( 'guide_description', $sizeguide->ID ); ?>
+                    <?php the_field('guide_description', $sizeguide->ID); ?>
                 </a>
 
                 <!-- Accordion tab content: it would start in the open state due to using the `is-active` state class. -->
                 <div class="accordion-content" data-tab-content>
-					<?php
-					$sizetable = get_field( 'table', $sizeguide->ID );
-					if ( $sizetable ) {
-						echo '<table>';
-						if ( $sizetable['header'] ) {
-							echo '<thead><tr>';
-							echo '';
-							foreach ( $sizetable['header'] as $th ) {
-								echo '<th class="text-center">';
-								echo $th['c'];
-								echo '</th>';
-							}
-							echo '</tr>';
-							echo '</thead>';
-						}
+                    <?php
+                    $sizetable = get_field('table', $sizeguide->ID);
+                    if ($sizetable) {
+                        echo '<table>';
+                        if ($sizetable['header']) {
+                            echo '<thead><tr>';
+                            echo '';
+                            foreach ($sizetable['header'] as $th) {
+                                echo '<th class="text-center">';
+                                echo $th['c'];
+                                echo '</th>';
+                            }
+                            echo '</tr>';
+                            echo '</thead>';
+                        }
 
-						echo '<tbody>';
-						foreach ( $sizetable['body'] as $tr ) {
-							echo '<tr>';
-							foreach ( $tr as $td ) {
-								echo '<td class="text-center">' . $td['c'] . '</td>';
-							}
-							echo '</tr>';
-						}
-						echo '</tbody>';
-						echo '</table>';
-					}
-					?>
+                        echo '<tbody>';
+                        foreach ($sizetable['body'] as $tr) {
+                            echo '<tr>';
+                            foreach ($tr as $td) {
+                                echo '<td class="text-center">' . $td['c'] . '</td>';
+                            }
+                            echo '</tr>';
+                        }
+                        echo '</tbody>';
+                        echo '</table>';
+                    }
+                    ?>
                 </div>
             </li>
             <!-- ... -->
         </ul>
     </div>
-	<?php
+    <?php
 }
 ?>
     <div class="woocommerce-variation-add-to-cart variations_button toprempad-1">
-		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+        <?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
-		<?php
-		do_action( 'woocommerce_before_add_to_cart_quantity' );
+        <?php
+        do_action('woocommerce_before_add_to_cart_quantity');
 
-		?>
+        ?>
         <div class="grid-x toppad-24">
             <div class="custom-quantity large-3 medium-5 small-6">
-				<?php
-				woocommerce_quantity_input( array(
-					'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-					'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-					'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(),
-					// WPCS: CSRF ok, input var ok.
-				) );
+                <?php
+                woocommerce_quantity_input(array(
+                    'min_value' => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+                    'max_value' => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+                    'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(),
+                    // WPCS: CSRF ok, input var ok.
+                ));
 
-				?>
+                ?>
 
-				<?php
-				do_action( 'woocommerce_after_add_to_cart_quantity' );
-				?>
+                <?php
+                do_action('woocommerce_after_add_to_cart_quantity');
+                ?>
             </div>
             <div class="large-9 medium-7 small-6 toppad-26 leftrempad-1">
                 <button type="submit"
                         class="single_add_to_cart_button button alt"
-                        style="width: 100%;"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+                        style="width: 100%;"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
             </div>
         </div>
 
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+        <?php do_action('woocommerce_after_add_to_cart_button'); ?>
 
-        <input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>"/>
-        <input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>"/>
+        <input type="hidden" name="add-to-cart" value="<?php echo absint($product->get_id()); ?>"/>
+        <input type="hidden" name="product_id" value="<?php echo absint($product->get_id()); ?>"/>
         <input type="hidden" name="variation_id" class="variation_id" value="0"/>
     </div>
-<?php if ( have_rows( 'sharing_links', 'option' ) ): ?>
+<?php if (have_rows('sharing_links', 'option')): ?>
     <div class="sharelinks grid-x toppad-8">
         <div class="large-2 medium-3 small-12 toppad-4">
             <span class="heebo ExtraBold fontsize-20">SHARE</span>
         </div>
-		<?php while ( have_rows( 'sharing_links', 'option' ) ): the_row(); ?>
+        <?php while (have_rows('sharing_links', 'option')): the_row(); ?>
 
-			<?php $icon = get_sub_field( 'icon' );
-			$iconURL    = $icon['url'];
-			?>
+            <?php $icon = get_sub_field('icon');
+            $iconURL = $icon['url'];
+            ?>
 
             <div class="large-1 medium-1 small-1">
-                <a href="<?php echo get_sub_field( 'link' );
-				the_permalink( $product->get_id() ); ?>" target="_blank"><img src="<?php echo $iconURL; ?>" style="max-width: 95%; width: 36px;"></a>
+                <a href="<?php echo get_sub_field('link');
+                the_permalink($product->get_id()); ?>" target="_blank"><img src="<?php echo $iconURL; ?>"
+                                                                            style="max-width: 95%; width: 36px;"></a>
             </div>
 
-		<?php endwhile; ?>
+        <?php endwhile; ?>
     </div>
 <?php endif; ?>
+<?php
+$add_a_logo_option = get_field('add_a_logo_option' , $product->get_id());
+if ($add_a_logo_option) {
+    ?>
+    <div class="add-a-logo toppad-16">
+        <img src="<?php echo get_template_directory_uri().'/assets/images/add-your-logo.png'; ?>" class="float-center">
+    </div>
+    <?php
+}
--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -16,114 +16,116 @@
  * @version 2.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 global $product;
 
-$tabCount                = 0;
-$tabsHeadings            = '';
-$tabsSections            = '';
-$productDescription      = $product->get_description();
+$tabCount = 0;
+$tabsHeadings = '';
+$tabsSections = '';
+$productDescription = $product->get_description();
 $productShortDescription = $product->get_short_description();
 
-if ( $productDescription != '' ) {
-	$tabCount ++;
-	$tabsHeadings .= '<li class="tabs-title is-active"><a href="#panel' . $tabCount . '" aria-selected="true"><p>'.get_field('epim_description_tab_heading','option').'</p></a></li>';
-	$tabsSections .= '<div class="tabs-panel is-active" id="panel' . $tabCount . '">';
-	$tabsSections .= '<div class="grid-x">';
-	$tabsSections .= $productDescription;
-	$tabsSections .= '</div></div>';
+if ($productDescription != '') {
+    $tabCount++;
+    $tabsHeadings .= '<li class="tabs-title is-active"><a href="#panel' . $tabCount . '" aria-selected="true"><p>' . get_field('epim_description_tab_heading', 'option') . '</p></a></li>';
+    $tabsSections .= '<div class="tabs-panel is-active" id="panel' . $tabCount . '">';
+    $tabsSections .= '<div class="grid-x">';
+    $tabsSections .= $productDescription;
+    $tabsSections .= '</div></div>';
 }
 
-if ( $productShortDescription != '' ) {
-	$tabCount ++;
-	if ( $tabCount == 1 ) {
-		$tabsHeadings .= '<li class="tabs-title is-active"><a href="#panel' . $tabCount . '" aria-selected="true"><p>'.get_field('epim_short_description_tab_heading','option').'</p></a></li>';
-	} else {
-		$tabsHeadings .= '<li class="tabs-title"><a href="#panel' . $tabCount . '"><p>Short Description from ePim</p></a></li>';
-	}
-	if ( $tabCount == 1 ) {
-		$tabsSections .= '<div class="tabs-panel is-active" id="panel' . $tabCount . '">';
-	} else {
-		$tabsSections .= '<div class="tabs-panel" id="panel' . $tabCount . '">';
-	}
-	$tabsSections .= '<div class="grid-x">';
-	$tabsSections .= $productShortDescription;
-	$tabsSections .= '</div></div>';
+if ($productShortDescription != '') {
+    $tabCount++;
+    if ($tabCount == 1) {
+        $tabsHeadings .= '<li class="tabs-title is-active"><a href="#panel' . $tabCount . '" aria-selected="true"><p>' . get_field('epim_short_description_tab_heading', 'option') . '</p></a></li>';
+    } else {
+        $tabsHeadings .= '<li class="tabs-title"><a href="#panel' . $tabCount . '"><p>Short Description from ePim</p></a></li>';
+    }
+    if ($tabCount == 1) {
+        $tabsSections .= '<div class="tabs-panel is-active" id="panel' . $tabCount . '">';
+    } else {
+        $tabsSections .= '<div class="tabs-panel" id="panel' . $tabCount . '">';
+    }
+    $tabsSections .= '<div class="grid-x">';
+    $tabsSections .= $productShortDescription;
+    $tabsSections .= '</div></div>';
 }
 
-if ( have_rows( 'product_tabs', $product->get_id() ) ):
+if (have_rows('product_tabs', $product->get_id())):
 
-?>
-<div class="product-tabs">
-	<?php
-	while ( have_rows( 'product_tabs', $product->get_id() ) ): the_row();
-		$tabCount ++;
-		$tabheadingClass = '';
-		if ( $tabCount == 1 ) {
-			$tabsHeadings .= '<li class="tabs-title is-active"><a href="#panel' . $tabCount . '" aria-selected="true"><p>' . get_sub_field( 'tab_heading' ) . '</p></a></li>';
-		} else {
-			$tabsHeadings .= '<li class="tabs-title"><a href="#panel' . $tabCount . '"><p>' . get_sub_field( 'tab_heading' ) . '</p></a></li>';
-		}
+    ?>
 
-		if ( $tabCount == 1 ) {
-			$tabsSections .= '<div class="tabs-panel is-active" id="panel' . $tabCount . '">';
-		} else {
-			$tabsSections .= '<div class="tabs-panel" id="panel' . $tabCount . '">';
-		}
-		$countColumns = count( get_sub_field( 'tab_columns' ) );
-		if ( have_rows( 'tab_columns' ) ):
+    <?php
+    while (have_rows('product_tabs', $product->get_id())): the_row();
+        $tabCount++;
+        $tabheadingClass = '';
+        if ($tabCount == 1) {
+            $tabsHeadings .= '<li class="tabs-title is-active"><a href="#panel' . $tabCount . '" aria-selected="true"><p>' . get_sub_field('tab_heading') . '</p></a></li>';
+        } else {
+            $tabsHeadings .= '<li class="tabs-title"><a href="#panel' . $tabCount . '"><p>' . get_sub_field('tab_heading') . '</p></a></li>';
+        }
 
-			$columnClass = '';
-			if ( $countColumns == 1 ) {
-				( $columnClass = ' class="large-12 medium-12 small-12" data-columns="' . $countColumns . '"' );
-			}
-			if ( $countColumns == 2 ) {
-				( $columnClass = ' class="large-6 medium-6 small-12 rightrempad-1" data-columns="' . $countColumns . '"' );
-			}
-			if ( $countColumns == 3 ) {
-				( $columnClass = ' class="large-4 medium-4 small-12 rightrempad-1" data-columns="' . $countColumns . '"' );
-			}
-			$tabsSections .= '<div class="grid-x">';
-			while ( have_rows( 'tab_columns' ) ): the_row();
-				$tab_column   = get_sub_field( 'tab_column' );
-				$tabsSections .= '<div' . $columnClass . '>' . $tab_column . '</div>';
-			endwhile;
-			$tabsSections .= '</div>';
-		endif;
-		$tabsSections .= '</div>';
-	endwhile;
-	endif;
-	if ( $tabsHeadings != '' ) {
-		?>
+        if ($tabCount == 1) {
+            $tabsSections .= '<div class="tabs-panel is-active" id="panel' . $tabCount . '">';
+        } else {
+            $tabsSections .= '<div class="tabs-panel" id="panel' . $tabCount . '">';
+        }
+        $countColumns = count(get_sub_field('tab_columns'));
+        if (have_rows('tab_columns')):
+
+            $columnClass = '';
+            if ($countColumns == 1) {
+                ($columnClass = ' class="large-12 medium-12 small-12" data-columns="' . $countColumns . '"');
+            }
+            if ($countColumns == 2) {
+                ($columnClass = ' class="large-6 medium-6 small-12 rightrempad-1" data-columns="' . $countColumns . '"');
+            }
+            if ($countColumns == 3) {
+                ($columnClass = ' class="large-4 medium-4 small-12 rightrempad-1" data-columns="' . $countColumns . '"');
+            }
+            $tabsSections .= '<div class="grid-x">';
+            while (have_rows('tab_columns')): the_row();
+                $tab_column = get_sub_field('tab_column');
+                $tabsSections .= '<div' . $columnClass . '>' . $tab_column . '</div>';
+            endwhile;
+            $tabsSections .= '</div>';
+        endif;
+        $tabsSections .= '</div>';
+    endwhile;
+endif;
+if ($tabsHeadings != '') {
+    ?>
+    <div class="product-tabs">
         <ul class="tabs" data-tabs id="example-tabs">
-			<?php echo $tabsHeadings; ?>
+            <?php echo $tabsHeadings; ?>
         </ul>
 
         <div class="tabs-content" data-tabs-content="example-tabs">
-			<?php echo $tabsSections; ?>
+            <?php echo $tabsSections; ?>
         </div>
-		<?php
-	}
-	?>
-</div>
+    </div>
+    <?php
+}
+?>
+
 <?php
-$wcTagTerms = get_the_terms( get_the_ID(), 'product_tag' );
-if ( $wcTagTerms && ! is_wp_error( $wcTagTerms ) ) :
+$wcTagTerms = get_the_terms(get_the_ID(), 'product_tag');
+if ($wcTagTerms && !is_wp_error($wcTagTerms)) :
 
-	$tagCells = array();
+    $tagCells = array();
 
-	foreach ( $wcTagTerms as $term ) {
-		$image      = get_field( 'image', $term );
-		$tagCells[] = '<div class="cell shrink"><img alt="' . $term->name . '" src="' . $image['url'] . '" style="margin-right: 12px;"/></div>';
-	}
+    foreach ($wcTagTerms as $term) {
+        $image = get_field('image', $term);
+        $tagCells[] = '<div class="cell shrink"><img alt="' . $term->name . '" src="' . $image['url'] . '" style="margin-right: 12px;"/></div>';
+    }
 
-	$on_draught = implode( "", $tagCells );
-	?>
+    $on_draught = implode("", $tagCells);
+    ?>
 
     <div class="grid-x grid-padding-y">
-		<?php echo $on_draught; ?>
+        <?php echo $on_draught; ?>
     </div>
 <?php endif; ?>
